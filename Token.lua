ab02341f17f4f863dd2c1c320b08f861ac11056c
Parihsz <65139606+Parihsz@users.noreply.github.com>
2023-06-24 06:31:54 -0400

    Update Token.lua

--- Token.lua
+++ Token.lua
@@ -1,9 +1,12 @@
 local Token = {}
 Token.__index = Token
 
-function Token.new(tokenType, value)
+function Token.new(tokenType, value, optional)
     local self = setmetatable({}, Token)
     self.type = tokenType
     self.value = value or 0
+    self.optional = optional or 0
     return self
 end
+
+return Token
