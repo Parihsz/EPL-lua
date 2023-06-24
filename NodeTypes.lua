9861d04c4ced7f3a660122ee9d980b5062db3c3b
Parihsz <65139606+Parihsz@users.noreply.github.com>
2023-06-24 06:26:53 -0400

    Update NodeTypes.lua

--- NodeTypes.lua
+++ NodeTypes.lua
@@ -2,7 +2,10 @@ local NodeTypes = {
     NewLineNode = -1,
     NumberNode = 0,
     BinOpNode = 1,
-    UnOpNode = 2
+    UnOpNode = 2,
+    NullNode = 3,
+    StringNode = 4,
+    BooleanNode = 5
 }
 
 return NodeTypes
