3688650fe6da8a7fd0012e973fe1e63c00ca96c2
Parihsz <65139606+Parihsz@users.noreply.github.com>
2023-06-24 06:27:37 -0400

    Update TokenTypes.lua

--- TokenTypes.lua
+++ TokenTypes.lua
@@ -1,12 +1,21 @@
 local TokenTypes = {
-    LPAREN = 6,
-    RPAREN = 7,
-    NUMBER = 1,
-    PLUS = 2,
-    MINUS = 3,
-    MULTIPLY = 4,
-    DIVIDE = 5,
-    NULL = 8
+    NEWLINE = 0,
+    PLUS = 1,
+    MINUS = 2,
+    MULTIPLY = 3,
+    DIVIDE = 4,
+    NULL = 5,
+    NUMBER = 6,
+    STRING = 7,
+    BOOL = 8,
+    LPAREN = 9,
+    RPAREN = 10,
+    POWER = 11,
+    KEYWORD = 12,
+    IDENTIFIER = 13,
+    LBRACKET = 14,
+    RBRACKET = 15,
+    FSTRING = 16
 }
 
 return TokenTypes
